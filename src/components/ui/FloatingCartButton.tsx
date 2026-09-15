fdc9201744651020678f72c901616783737d3784
GENAI-NETWORKS <genaitechnology@gmail.com>
2026-09-15 22:33:24 +0530

    Reduce FloatingCartButton width to decrease center gap

--- src/components/ui/FloatingCartButton.tsx
+++ src/components/ui/FloatingCartButton.tsx
@@ -20,7 +20,7 @@ export function FloatingCartButton() {
 
   return (
     <div className="fixed bottom-[80px] lg:bottom-6 left-0 right-0 z-[100000] px-4 pointer-events-none">
-      <div className="max-w-[600px] mx-auto pointer-events-auto">
+      <div className="max-w-[340px] mx-auto pointer-events-auto">
         <Link
           href="/cart"
           className="flex items-center justify-between bg-[#0f8a3c] rounded-[16px] px-4 py-3 shadow-[0_8px_24px_rgba(15,138,60,0.35)] active:scale-[0.98] transition-transform"
